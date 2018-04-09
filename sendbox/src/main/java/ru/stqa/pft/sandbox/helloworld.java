96b1299b70e604618ec513b3666ee5e1bb55ae90
Ilya <IGrushevsky@bellintegrator.com>
2018-04-09 13:03:02 +0300

    обновление 1.1

--- sendbox/src/main/java/ru/stqa/pft/sandbox/helloworld.java
+++ sendbox/src/main/java/ru/stqa/pft/sandbox/helloworld.java
@@ -3,5 +3,9 @@ package ru.stqa.pft.sandbox;
 public class helloworld {
 	public static void main(String[] args) {
 		System.out.println("Hello, world!");
+
+		int l = 9;
+		int s = (l * l);
+		System.out.println("площадь квадрата со стороной " + l + " равна " + s);
 	}
 }
